3ae16cadbe72b6882642e409bcc2959958c14c28
Justin <tribekiller911@gmail.com>
2017-09-26 16:53:24 -0700

    Generating Names

--- ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Program.cs
+++ ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Program.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bogus.Extensions.Canada;
+using Bogus;
+using ConsoleDump;
 
 namespace ServerForTheLogic {
     class Program {
         static void Main(string[] args) {
+            Person me = new Person();
+            me.createPerson();
+            me.Dump();
+            me.KeepOpen();
         }
+        public class Person {
+            public string FName { get; set; }
+            public string LName { get; set; }
 
-        public void DateofirthB(){
-            Random rnd = new Random();
-            int month = rnd.Next(1, 13);  // month: >= 1 and < 13
-            
+            public void createPerson() {
+                var modelFaker = new Faker<Person>()
+                    .RuleFor(o => o.FName, f => f.Name.FirstName())
+                    .RuleFor(o => o.LName, f => f.Name.LastName());
+
+                var Human = modelFaker.Generate();
+                FName = Human.FName;
+                LName = Human.LName;
+                //Human.Dump();
+            }
+            public void KeepOpen() {
+                while (true) {
+                }
+            }
+
+
         }
     }
 }
