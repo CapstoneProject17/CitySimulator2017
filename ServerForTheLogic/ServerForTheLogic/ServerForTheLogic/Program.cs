91798e25f2b50be25392469a146ef518fe1136db
Justin <tribekiller911@gmail.com>
2017-09-26 16:07:57 -0700

    Added Bogus

--- ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Program.cs
+++ ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Program.cs
@@ -8,5 +8,12 @@ namespace ServerForTheLogic {
     class Program {
         static void Main(string[] args) {
         }
+
+        public void DateofirthB(){
+            Random rnd = new Random();
+            int month = rnd.Next(1, 13);  // month: >= 1 and < 13
+            
+        }
     }
 }
+        
