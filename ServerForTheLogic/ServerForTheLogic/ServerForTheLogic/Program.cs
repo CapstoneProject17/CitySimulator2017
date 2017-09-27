a02687cec9bf4a97a25853725bd809c913a8fdf4
Justin <tribekiller911@gmail.com>
2017-09-26 17:18:51 -0700

    People List
    Messy Needs cleaning

--- ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Program.cs
+++ ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Program.cs
@@ -9,12 +9,23 @@ using ConsoleDump;
 
 namespace ServerForTheLogic {
     class Program {
+        public static List<Person> People = new List<Person>();
+
         static void Main(string[] args) {
+            //Person me = new Person();
+            //me.Dump();
             Person me = new Person();
-            me.createPerson();
-            me.Dump();
+            for (int i = 0; i < 24; i++) {
+                me = new Person();
+                me.createPerson();
+                People.Add(me);
+                me = null;
+            }
+            People.Dump();
+            me = new Person();
             me.KeepOpen();
         }
+
         public class Person {
             public string FName { get; set; }
             public string LName { get; set; }
