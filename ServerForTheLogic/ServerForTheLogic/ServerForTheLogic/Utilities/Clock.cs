02051d5ba1f06bc9db1b5e10c3580bce27b7ae7b
unhouseledutraquist <andrewlbusto@gmail.com>
2017-10-02 22:52:31 -0700

    Clock.cs commented.

--- ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Utilities/Clock.cs
+++ ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Utilities/Clock.cs
@@ -7,11 +7,20 @@ using System.Timers;
 
 namespace ServerForTheLogic.Utilities
 {
+    /// <summary>
+    /// Holds the current time.  Intended for use by the City.
+    /// <para/> Last edited:  2017-10-02
+    /// </summary>
     class Clock
     {
+        // Ticks every second to update the current time values.
         private Timer timer;
 
+        
         private UInt32 netMinutes;
+        /// <summary>
+        /// The total number of minutes since this Clock started.
+        /// </summary>
         public UInt32 NetMinutes
         {
             get
@@ -20,6 +29,9 @@ namespace ServerForTheLogic.Utilities
             }
         }
 
+        /// <summary>
+        /// The number of minutes that have passed in the current hour.
+        /// </summary>
         public UInt32 MinuteComponent
         {
             get
@@ -29,6 +41,9 @@ namespace ServerForTheLogic.Utilities
         }
 
         private UInt32 netHours;
+        /// <summary>
+        /// The total number of hours since this Clock started.
+        /// </summary>
         public UInt32 NetHours
         {
             get
@@ -37,6 +52,9 @@ namespace ServerForTheLogic.Utilities
             }
         }
 
+        /// <summary>
+        /// The number of hours that have passed in the current day.
+        /// </summary>
         public UInt32 HourComponent
         {
             get
@@ -46,6 +64,9 @@ namespace ServerForTheLogic.Utilities
         }
 
         private UInt32 netDays;
+        /// <summary>
+        /// The total number of days since this Clock started.
+        /// </summary>
         public UInt32 NetDays
         {
             get
@@ -54,8 +75,15 @@ namespace ServerForTheLogic.Utilities
             }
         }
 
-        public const int INTERVAL = 100;
+        /// <summary>
+        /// The number of milliseconds between Clock "ticks."  In this case, 1 second.
+        /// </summary>
+        public const int INTERVAL = 1000;
 
+        /// <summary>
+        /// Constructs a Clock object.
+        /// <para/> Last edited:  2017-10-02
+        /// </summary>
         public Clock()
         {
             timer = new Timer();
@@ -69,6 +97,12 @@ namespace ServerForTheLogic.Utilities
             timer.Start();
         }
 
+        /// <summary>
+        /// Increments netMins.  Set as an event handler for timer.
+        /// </summary>
+        /// <para/> Last edited:  2017-10-02
+        /// <param name="source"> Unused. </param>
+        /// <param name="e"> Unused .</param>
         private void tickMinute(Object source, ElapsedEventArgs e)
         {
             netMinutes++;
@@ -80,6 +114,10 @@ namespace ServerForTheLogic.Utilities
             }
         }
 
+        /// <summary>
+        /// Updates netHours.
+        /// </summary>
+        /// <para/> Last edited:  2017-10-02
         private void tickHour()
         {
             netHours = netMinutes / 60;
@@ -91,6 +129,10 @@ namespace ServerForTheLogic.Utilities
             }
         }
 
+        /// <summary>
+        /// Updates netDays.
+        /// </summary>
+        /// <para/> Last edited:  2017-10-02
         private void tickDay()
         {
             netDays = netHours / 24;
