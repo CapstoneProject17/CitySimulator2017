a1e6e7aa7339b0b580811c5d38befe677b6a7dca
Michael O'Connell <m.t.oconnell@hotmail.com>
2017-10-15 18:23:57 -0700

    Created the MongoDB DAL with some standard database methods for the Citizen objects. Other Objects not done yet.

---
+++ ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Buildings.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace DataAccessLayer {
+    class Buildings {
+        public ObjectId Id { get; set; }
+        public string BuildingName { get; set; }
+        public string BuildingType { get; set; }
+        public string BuildingCompany { get; set; }
+        public int BuildingLocation { get; set; }
+        public int BuildingMoney { get; set; }
+        public int BuildingLevel { get; set; }
+    }
+}
---
+++ ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Roads.cs
@@ -0,0 +1,8 @@
+﻿using MongoDB.Bson;
+
+namespace DataAccessLayer {
+    class Roads {
+        public ObjectId Id { get; set; }
+        public int RoadCoordinates { get; set; }
+    }
+}
