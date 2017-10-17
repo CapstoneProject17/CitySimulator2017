d33480817a5d4fd96fede82648f43f56eaf18060
haro-freezd <harmanmahal@gmail.com>
2017-10-17 00:18:52 -0700

    Initial scaffolding for DatabaseHandler and moved files

---
+++ CitySimNetworkService/CitySimNetworkService/Handlers/DatabaseHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Json;
+
+namespace CitySimNetworkService
+{
+    internal class DatabaseHandler
+    {
+
+        internal JsonObject HandleRequest(DatabaseResourceRequest request)
+        {
+            switch (request.ResourceType)
+            {
+                case "building":
+                //Call the function to get building info
+                case "person":
+                //call the function to get person info
+                default:
+                    return new JsonObject();
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
