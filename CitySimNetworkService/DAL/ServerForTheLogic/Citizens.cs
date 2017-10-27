1e63b6acfe82d0e191cee2ad31d3640fe63d8649
Francis Carreon <fcarreon1@my.bcit.ca>
2017-10-27 15:49:03 -0700

    Imported Database project into solution
    > added DAL folder and contents
    > added DAL.sln to CitySimNetworkService.sln

---
+++ CitySimNetworkService/DAL/ServerForTheLogic/Citizens.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace DataAccessLayer {
+    class Citizens {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int HomeAddress { get; set; }
+        public int WorkAddress { get; set; }
+        public int Age { get; set; }
+        public int DaysLeftToLive { get; set; } // Prophetic
+    }
+}
