490968f4f7d0ae8daf3e56813e6faea20478e5ba
scosacescu <stephanie.cosacescu@gmail.com>
2017-11-08 22:10:50 -0800

    Updated DAL classes to reflect updated "schema"

---
+++ ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerForTheLogic
+{
+    /// <summary>
+    /// MongoDAL
+    /// Team: DB
+    /// Products being bought/sold within the simulated economy.
+    /// Author: Stephanie 
+    /// Date: 2017 11 08
+    /// </summary>
+    class Product
+    {
+        public string Name { get; set; }
+        public int GlobalCountr { get; set; }
+    }
+}
