469c4e3914d5db7587098012958b256429bb7c1b
Connor Goudie <connorgoudie@live.com>
2017-11-21 19:29:18 -0800

    Can now run application as either a console app or service
    If you need to know how to run it as a service, ask me.
    Added a logging library so we can debug when running the application as a service.

--- ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Program.cs
+++ ServerForTheLogic/ServerForTheLogic/ServerForTheLogic/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Bogus.Extensions.Canada;
-using Bogus;
 using ConsoleDump;
-using ServerForTheLogic.Utilities;
 using ServerForTheLogic.Infrastructure;
 using Newtonsoft.Json;
 using ServerForTheLogic.Json;
 using ServerForTheLogic.Econ;
+using System.ServiceProcess;
+using NLog;
 
 namespace ServerForTheLogic
 {
@@ -19,19 +15,16 @@ namespace ServerForTheLogic
     /// </summary>
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public const string ServiceName = "ServerForTheLogic";
         public static List<Person> People = new List<Person>();
         private const int MEAN_DEATH_AGE = 80;
         private const int STANDARD_DEVIATION_DEATH = 14;
         private static City city;
 
-        /// <summary>
-        /// Entry point for the city simulator program
-        /// <para/> Last editted:  2017-10-02
-        /// </summary>
-        /// <param name="args"></param>
-        static void Main(string[] args)
-        {
 
+        public static void Start(string[] args)
+        {
             DatabaseLoader loader = new DatabaseLoader();
             city = loader.loadCity();
             // Block b, b1, b2;
@@ -49,7 +42,93 @@ namespace ServerForTheLogic
             updater.sendFullUpdate(city, Formatting.Indented);
             //foo();
             test2();
-            while (true) {
+            GetInput();
+        }
+
+        public static void Stop()
+        {
+            // onstop code here
+        }
+
+        /// <summary>
+        /// Entry point for the city simulator program
+        /// <para/> Last editted:  2017-10-02
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            logger.Info("its a bug");
+            logger.Info("its a bug");
+            logger.Info("its a bug");
+            logger.Info("its a bug");
+            if (!Environment.UserInteractive)
+            {
+                Console.WriteLine("service");
+                // running as service
+                using (var service = new Service())
+                    ServiceBase.Run(service);
+            }
+            else
+            {
+                // running as console app
+                Console.WriteLine("console");
+                DatabaseLoader loader = new DatabaseLoader();
+                city = loader.loadCity();
+                // Block b, b1, b2;
+                if (city == null)
+                {
+                    //TEST DATA 
+                    city = new City();
+                    //fill 3 blocks
+
+                }
+
+                //city.printBlockMapTypes();
+                city.printCity();
+                Updater<City> updater = new Updater<City>();
+                updater.sendFullUpdate(city, Formatting.Indented);
+                //foo();
+                test2();
+                GetInput();
+            }
+        }
+        /// <summary>
+        /// Keeps the program running 
+        /// <para/> Last editted:  2017-10-02
+        /// </summary>
+        //public static void KeepOpen()
+        //{
+        //    while (true) ;
+        //}
+
+
+        /// <summary>
+        /// Test method for Market transaction (Person)
+        /// </summary>
+        private static void test1()
+        {
+            Console.WriteLine("FOO");
+            Person p = city.createPerson();
+            Console.WriteLine("funds before " + p.Funds);
+            p.BuyThings();
+            Console.WriteLine("funds after " + p.Funds);
+        }
+        /// <summary>
+        /// Test method for Market transaction (Commercial)
+        /// </summary>
+        private static void test2()
+        {
+            Console.WriteLine("BAR");
+            Commercial b = new Commercial("new", 10,true);
+            Console.WriteLine("funds before "  + b.Funds);
+            b.FillInventory();
+            //Console.WriteLine("funds after " + b.Funds);
+        }
+
+        private static void GetInput()
+        {
+            while (true)
+            {
                 Console.WriteLine("Enter Command:");
                 String cmd = Console.ReadLine();
                 //Console.WriteLine(cmd);
@@ -89,38 +168,6 @@ namespace ServerForTheLogic
                 if (commands[0].Equals("resume", StringComparison.CurrentCultureIgnoreCase))
                     city.clock.timer.Start();
             }
-        }
-        /// <summary>
-        /// Keeps the program running 
-        /// <para/> Last editted:  2017-10-02
-        /// </summary>
-        //public static void KeepOpen()
-        //{
-        //    while (true) ;
-        //}
-
-
-        /// <summary>
-        /// Test method for Market transaction (Person)
-        /// </summary>
-        private static void test1()
-        {
-            Console.WriteLine("FOO");
-            Person p = city.createPerson();
-            Console.WriteLine("funds before " + p.Funds);
-            p.BuyThings();
-            Console.WriteLine("funds after " + p.Funds);
-        }
-        /// <summary>
-        /// Test method for Market transaction (Commercial)
-        /// </summary>
-        private static void test2()
-        {
-            Console.WriteLine("BAR");
-            Commercial b = new Commercial("new", 10,true);
-            Console.WriteLine("funds before "  + b.Funds);
-            b.FillInventory();
-            //Console.WriteLine("funds after " + b.Funds);
         }
 
     }
