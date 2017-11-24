6eeb7a6c3febc2f13efdac5a37d666bc30e440a3
Kevin Mitchell <you@example.com>
2017-11-24 12:19:15 -0800

    changed DAL to only instantiate database connection once

--- CitySimNetworkService/CitySimNetworkApplication/Handlers/DatabaseHandler.cs
+++ CitySimNetworkService/CitySimNetworkApplication/Handlers/DatabaseHandler.cs
@@ -6,54 +6,60 @@ using DataAccessLayer;
 
 namespace CitySimNetworkService
 {
-    /// <summary> 
-    /// Contains handler for database request. 
-    /// </summary>
-    /// 
-    /// <author> 
-    /// Harman Mahal, Kevin Mitchell
-    /// </author>
-    public class DatabaseHandler
-    {
-        MongoDAL data;
+	/// <summary> 
+	/// Contains handler for database request. 
+	/// </summary>
+	/// 
+	/// <author> 
+	/// Harman Mahal, Kevin Mitchell
+	/// </author>
+	public class DatabaseHandler
+	{
+		/// <summary>
+		/// MongoDAL used to call Database methods
+		/// </summary>
+		MongoDAL data;
 
-        public DatabaseHandler()
-        {
-            data = new MongoDAL();
-        }
+		/// <summary>
+		/// DatabaseHandler is instantiated in Program
+		/// </summary>
+		public DatabaseHandler()
+		{
+			data = new MongoDAL();
+		}
 
-        /// <summary> 
-        /// Handler; returns JSON object containing Buildings/Citizens information on valid request. 
-        /// </summary>
-        /// 
-        /// <param name="request">
-        /// Contains resource type and resource id. 
-        /// </param>
-        /// 
-        /// <returns> 
-        /// JSON object; has Buildings/Citizens/empty information. 
-        /// </returns>
-        internal string HandleRequest(DatabaseResourceRequest request)
-        {
-            switch (request.ResourceType)
-            {
-                case "object":
-                    Guid guid = new Guid(request.ResourceID);   //parse resourceID into GUID
-                    Object obj = data.GetObjectByGuid(guid);           //get object from database via GUID
-                    return JsonConvert.SerializeObject(obj);    //serialize object as JSON and return to client
+		/// <summary> 
+		/// Handler; returns JSON object containing Buildings/Citizens information on valid request. 
+		/// </summary>
+		/// 
+		/// <param name="request">
+		/// Contains resource type and resource id. 
+		/// </param>
+		/// 
+		/// <returns> 
+		/// JSON object; has Buildings/Citizens/empty information. 
+		/// </returns>
+		internal string HandleRequest(DatabaseResourceRequest request)
+		{
+			switch (request.ResourceType)
+			{
+				case "object":
+					Guid guid = new Guid(request.ResourceID);   //parse resourceID into GUID
+					Object obj = data.GetObjectByGuid(guid);           //get object from database via GUID
+					return JsonConvert.SerializeObject(obj);    //serialize object as JSON and return to client
 
-                case "product":
-                    Object product = data.GetProduct(request.ResourceID);
-                    return JsonConvert.SerializeObject(product);
+				case "product":
+					Object product = data.GetProduct(request.ResourceID);
+					return JsonConvert.SerializeObject(product);
 
-                case "clock":
-                    Object clock = data.GetClock();
-                    return JsonConvert.SerializeObject(clock);
+				case "clock":
+					Object clock = data.GetClock();
+					return JsonConvert.SerializeObject(clock);
 
-                default:
-                    string errorString = "{Request: 'invalid'}";
-                    return errorString;
-            }
-        }
-    }
+				default:
+					string errorString = "{Request: 'invalid'}";
+					return errorString;
+			}
+		}
+	}
 }
