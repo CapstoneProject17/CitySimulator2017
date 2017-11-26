065a449ac84662afba73cc0fa447dd27e6e70f98
Benjamin <haomingkai@gmail.com>
2017-11-25 21:34:59 -0800

    Map Update

---
+++ Client/CitySimulator_Client/Assets/Scripts/HUD/MapFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Module: MapFollow
+/// Team: Client
+/// Description: Make Map follow the main camera
+/// Author: Benjamin Hao Date: Nov. 23rd, 2017
+/// Modified by: N/A
+/// Based on:  Creating a Basic MiniMap in Unity 3D
+///            https://www.youtube.com/watch?v=ZuV9Xlt-l6g&t=29s
+/// </summary>
+/// 
+public class MapFollow : MonoBehaviour
+{
+    public Transform Target;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(Target.position.x, transform.position.y, Target.position.z);
+    }
+}
