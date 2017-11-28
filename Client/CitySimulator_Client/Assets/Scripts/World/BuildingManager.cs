382d7e097e7b416f376683c2ce675a334e4c5b21
Andrew Lam <atlam995@gmail.com>
2017-11-28 11:53:13 -0800

    Fixed industrial1 buildings transparency problem

--- Client/CitySimulator_Client/Assets/Scripts/World/BuildingManager.cs
+++ Client/CitySimulator_Client/Assets/Scripts/World/BuildingManager.cs
@@ -199,6 +199,10 @@ public class BuildingManager : MonoBehaviour {
 		}
 	}
 
+	void rotateBuilding(GameObject obj) {
+	}
+
+
 	// Instantiates the building object
 	void instantiateBuilding (GameObject obj, float x, float y, float z) {
 
