d2e4c1ed19afa009d3d5db1223340721824b8252
haro-freezd <harmanmahal@gmail.com>
2017-11-27 22:51:50 -0800

    Finished server logic integration in server service

--- CitySimNetworkService/CitySimNetworkService/NetworkService.cs
+++ CitySimNetworkService/CitySimNetworkService/NetworkService.cs
@@ -29,6 +29,14 @@ namespace CitySimNetworkService
         private AsyncServer connectionHandler;
         private City city;
         private Updater<City> updater;
+        SimulationStateQueue fullUpdateQueue = new SimulationStateQueue
+        {
+            StateBufferSize = 1
+        };
+        SimulationStateQueue partialUpdateQueue = new SimulationStateQueue
+        {
+            StateBufferSize = 25
+        };
 
         /// <summary>
         /// Initializes application network objects.
@@ -36,20 +44,11 @@ namespace CitySimNetworkService
         public NetworkService()
         {
             InitializeComponent();
-            SimulationStateQueue fullUpdateQueue = new SimulationStateQueue
-            {
-                StateBufferSize = 1
-            };
-            SimulationStateQueue partialUpdateQueue = new SimulationStateQueue
-            {
-                StateBufferSize = 25
-            };
-
             SimulationStateHandler simulationHandler = new SimulationStateHandler(partialUpdateQueue, fullUpdateQueue);
             DatabaseHandler dbHandler = new DatabaseHandler();
             RequestHandler requestHandler = new RequestHandler(dbHandler, simulationHandler);
             connectionHandler = new AsyncServer(requestHandler);
-            
+
             if (city == null)
             {
                 city = new City(fullUpdateQueue, partialUpdateQueue);
@@ -66,7 +65,7 @@ namespace CitySimNetworkService
         protected override void OnStart(string[] args)
         {
             connectionHandler.StartListening();
-            //Call city start here 
+            city.StartSimulation(fullUpdateQueue, partialUpdateQueue);
         }
 
         /// <summary>
