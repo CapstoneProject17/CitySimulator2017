749fad9106f384f336b5adcc35f180adfa37b374
haro-freezd <harmanmahal@gmail.com>
2017-11-28 11:35:21 -0800

    Adding client side network integration

--- Client/CitySimulator_Client/Assets/Scripts/Network/NetworkConnectionHandler.cs
+++ Client/CitySimulator_Client/Assets/Scripts/Network/NetworkConnectionHandler.cs
@@ -20,146 +20,157 @@ using System.Text;
 /// What the superviosr should know: N/A
 /// </summary>
 /// 
-public class NetworkConnectionHandler {
+public static class NetworkConnectionHandler
+{
+    public static bool socketReady = false;
+    public static TcpClient socket;
+    public static NetworkStream stream;
+    public static StringBuilder sb = new StringBuilder();
+    public static string json;
+    public static string host = "127.0.0.1";
+    public static int port = 13456;
 
-//	public GameObject chatContainer;
-//	public GameObject messagePrefab;
+    public static void ConnectToServer()
+    {
+        if (socketReady)
+        {
+            return;
+        }
 
-	public string clientName;
+        //default host / port values
+        string host = "127.0.0.1";
+        int port = 13456;
 
-	public static bool socketReady = false;
-	public static TcpClient socket;
-	public static NetworkStream stream;
-	public static StringBuilder sb = new StringBuilder();
-	public static string json;
-	public static string host = "127.0.0.1";
-	public static int port = 13456;
+        //		//overwrite default host / port values, if there is something in thoese boxes
+        //		string h;
+        //		int p;
+        //
+        //		if (h != "")
+        //			host = h;
+        //		if (p != 0)
+        //		{
+        //			port = p;
+        //		}
 
-	public static void ConnectToServer()
-	{
-		if (socketReady)
-		{
-			return;
-		}
-
-		//default host / port values
-		string host = "127.0.0.1";
-		int port = 13456;
-
-//		//overwrite default host / port values, if there is something in thoese boxes
-//		string h;
-//		int p;
-//
-//		if (h != "")
-//			host = h;
-//		if (p != 0)
-//		{
-//			port = p;
-//		}
-
-		//cresate the socket
-		try
-		{
-			socket = new TcpClient(host, port);
-			stream = socket.GetStream();
-			socketReady = true;
-		}
-		catch (Exception e)
-		{
-			Debug.Log("Socket errror: " + e.Message);
-		}
-	}
+        //cresate the socket
+        try
+        {
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+            socketReady = true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket errror: " + e.Message);
+        }
+    }
 
 
-	public static void ReadFromServer() {
-		if (socketReady)
-		{
-			if (stream.DataAvailable)
-			{
-				byte[] buffer = new byte[1024];
-				int data = stream.Read(buffer, 0, 1024);
-				if (data != 0)
-				{
-					//create json string
-					json = JsonUtility.ToJson(data);
-					
-				}
-				stream.Flush();
-			}
-		}
-	}
+    public static string ReadFromServer()
+    {
+        if (socketReady)
+        {
+            if (stream.DataAvailable)
+            {
+                byte[] buffer = new byte[stream.Length];
+                int data = stream.Read(buffer, 0, buffer.Length);
+                if (data != 0)
+                {
+                    string readData = Convert.ToBase64String(buffer);
+                    //create json string
+                    json = JsonUtility.ToJson(readData);
 
-	public static void WriteForServer(string data) {
-		if (!socketReady)
-		{
-			Debug.Log("Socket not ready");
-			Console.WriteLine("Socket not ready");
-			return;
-		}
+                }
+                stream.Flush();
 
+            }
+        }
 
-		byte[] dataToSend = Encoding.ASCII.GetBytes(data);
-		stream.Write(dataToSend, 0, dataToSend.Length);
-		stream.Flush();
+        CloseSocket();
+        return json;
+    }
 
-		Debug.Log("data sent");
-	}
+    public static string WriteForServer(string data)
+    {
+        if (!socketReady)
+        {
+            Debug.Log("Socket not ready");
+            Console.WriteLine("Socket not ready");
+            ConnectToServer();
+        }
+        byte[] dataToSend = Encoding.ASCII.GetBytes(data);
+        stream.Write(dataToSend, 0, dataToSend.Length);
+        stream.Flush();
 
+        Debug.Log("data sent");
 
-	private void OnIncomingData(string data)
-	{	
-		//        if (data == "%NAME")
-		//        {
-		//            Send("&NAME| " + clientName);
-		//            return;
-		//        }
-
-		//detect data and parse into an object
-		DataManager.ToObject(data);
+        return ReadFromServer();
+    }
 
 
-	}
+    private static void OnIncomingData(string data)
+    {
+        //        if (data == "%NAME")
+        //        {
+        //            Send("&NAME| " + clientName);
+        //            return;
+        //        }
 
-	/// <summary>
-	///  check whether or not a client is connected
-	/// </summary>
-	private bool isConnected(TcpClient c) {
-		try {
-			if(c != null && c.Client != null && c.Client.Connected ) {
-				if(c.Client.Poll(0, SelectMode.SelectRead)) 
-					return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
-				return true;
-			}else 
-				return false;
-		}catch {
-			return false;
-		}
-	}
+        //detect data and parse into an object
+        DataManager.ToObject(data);
 
-	/// <summary>
-	/// Closes the socket.
-	/// </summary>
-	private void CloseSocket() {
-		if (!socketReady)
-			return;
 
-		stream.Close ();
-		socket.Close ();
-		socketReady = false;
-	}
-	/// <summary>
-	/// Raises the application quit event.
-	/// </summary>
-	private void OnApplicationQuit() {
-		CloseSocket ();
-	}
+    }
 
-	/// <summary>
-	/// Raises the disable event.
-	/// </summary>
-	private void OnDisable() {
-		CloseSocket ();
-	}
+    /// <summary>
+    ///  check whether or not a client is connected
+    /// </summary>
+    private static bool IsConnected(TcpClient c)
+    {
+        try
+        {
+            if (c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+                return true;
+            }
+            else
+                return false;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Closes the socket.
+    /// </summary>
+    private static void CloseSocket()
+    {
+        if (!socketReady)
+            return;
+
+        stream.Close();
+        socket.Close();
+        socketReady = false;
+    }
+    /// <summary>
+    /// Raises the application quit event.
+    /// </summary>
+    private static void OnApplicationQuit()
+    {
+        CloseSocket();
+    }
+
+    /// <summary>
+    /// Raises the disable event.
+    /// </summary>
+    private static void OnDisable()
+    {
+        CloseSocket();
+    }
 
 }
 
--- Client/CitySimulator_Client/Assets/Scripts/World/CityDataManager.cs
+++ Client/CitySimulator_Client/Assets/Scripts/World/CityDataManager.cs
@@ -16,7 +16,8 @@ using UnityEngine;
 /// </summary>
 
 [Serializable]
-public class Point{
+public class Point
+{
 
     // x coordinate 
     public int x;
@@ -36,7 +37,8 @@ public class Point{
 /// Based on:  N/A
 /// </summary>
 [Serializable]
-public class NewBuilding{
+public class NewBuilding
+{
 
     // unique id for the building
     public string id;
@@ -69,14 +71,15 @@ public class NewBuilding{
 /// Based on:  N/A
 /// </summary>
 [Serializable]
-public class CityData{
+public class CityData
+{
 
     // length of the grid for the city
     public int GridLength;
 
     // width of the grid for the city
     public int GridWidth;
-    
+
     // hour for the city
     public int netHours;
 
@@ -102,136 +105,163 @@ public class CityData{
 ///  Name Lancelei Herradura	Change: Add human dictionary Date: 2017-11-25
 /// Based on:  N/A
 /// </summary>
-public class CityDataManager : MonoBehaviour {
+public class CityDataManager : MonoBehaviour
+{
 
     // JSON dummy String for testing
     private static string jsonString = "{\"GridLength\":99,\"GridWidth\":58,\"netHours\":0,\"NewRoads\":[{\"x\":27,\"z\":49},{\"x\":27,\"z\":56},{\"x\":28,\"z\":49},{\"x\":28,\"z\":56},{\"x\":29,\"z\":49},{\"x\":29,\"z\":56},{\"x\":30,\"z\":49},{\"x\":30,\"z\":56},{\"x\":27,\"z\":50},{\"x\":30,\"z\":50},{\"x\":27,\"z\":51},{\"x\":30,\"z\":51},{\"x\":27,\"z\":52},{\"x\":30,\"z\":52},{\"x\":27,\"z\":53},{\"x\":30,\"z\":53},{\"x\":27,\"z\":54},{\"x\":30,\"z\":54},{\"x\":27,\"z\":55},{\"x\":30,\"z\":55},{\"x\":24,\"z\":56},{\"x\":24,\"z\":63},{\"x\":25,\"z\":56},{\"x\":25,\"z\":63},{\"x\":26,\"z\":56},{\"x\":26,\"z\":63},{\"x\":27,\"z\":63},{\"x\":24,\"z\":57},{\"x\":27,\"z\":57},{\"x\":24,\"z\":58},{\"x\":27,\"z\":58},{\"x\":24,\"z\":59},{\"x\":27,\"z\":59},{\"x\":24,\"z\":60},{\"x\":27,\"z\":60},{\"x\":24,\"z\":61},{\"x\":27,\"z\":61},{\"x\":24,\"z\":62},{\"x\":27,\"z\":62},{\"x\":21,\"z\":63},{\"x\":21,\"z\":70},{\"x\":22,\"z\":63},{\"x\":22,\"z\":70},{\"x\":23,\"z\":63},{\"x\":23,\"z\":70},{\"x\":24,\"z\":70},{\"x\":21,\"z\":64},{\"x\":24,\"z\":64},{\"x\":21,\"z\":65},{\"x\":24,\"z\":65},{\"x\":21,\"z\":66},{\"x\":24,\"z\":66},{\"x\":21,\"z\":67},{\"x\":24,\"z\":67},{\"x\":21,\"z\":68},{\"x\":24,\"z\":68},{\"x\":21,\"z\":69},{\"x\":24,\"z\":69}],\"NewBuildings\":[{\"id\":\"24132329-e85a-4072-b9c8-1dab463b8443\",\"Name\":\"Pacocha Inc\",\"Point\":{\"x\":28,\"z\":54},\"Type\":\"I\",\"Rating\":0,\"IsTall\":true},{\"id\":\"0a6a8518-fc33-4d7d-bf88-ef7464f72d5e\",\"Name\":\"Hilll, Kohler and Effertz\",\"Point\":{\"x\":25,\"z\":59},\"Type\":\"C\",\"Rating\":0,\"IsTall\":true},{\"id\":\"43018e9e-b03b-45d1-b214-ae7a623d5a8a\",\"Name\":\"Residence\",\"Point\":{\"x\":22,\"z\":69},\"Type\":\"H\",\"Rating\":0,\"IsTall\":true}]}";
-    
+    private string initialCityState;
+    private string partialCityState;
+    private int? lastUpdate;
+
     // deserialize json to object
     private CityData cityData = JsonUtility.FromJson<CityData>(jsonString);
-	
+
     // switch for testing
-	public bool turnOnTestGrid;
-	// update trigger
-	public bool updateTheCity;
+    public bool turnOnTestGrid;
+    // update trigger
+    public bool updateTheCity;
 
-	// simulator's TimeStamp for start: hours + minute/60 + second/60
-	public double systemStartedTimeStamp;
+    // simulator's TimeStamp for start: hours + minute/60 + second/60
+    public double systemStartedTimeStamp;
 
-	// simualtor's TimeStampe for current: hours + minute/60 + second/60
-	public double systemCurrentTimeStamp;
+    // simualtor's TimeStampe for current: hours + minute/60 + second/60
+    public double systemCurrentTimeStamp;
 
-	// timeInHour
-	public double timeInHour;
+    // timeInHour
+    public double timeInHour;
 
-	// population of the city
-	private int population = 1000;
+    // population of the city
+    private int population = 1000;
 
-	/// <summary>
-	/// Gets the population.
-	/// </summary>
-	/// <returns>The population.</returns>
-	public int Population{
-		get {
-			return  population;
-		}
-		set {
-			population = value;
-		}
-	}
+    /// <summary>
+    /// Gets the population.
+    /// </summary>
+    /// <returns>The population.</returns>
+    public int Population
+    {
+        get
+        {
+            return population;
+        }
+        set
+        {
+            population = value;
+        }
+    }
 
-	// x number of grids in horizontal
-	private int size_x;
+    // x number of grids in horizontal
+    private int size_x;
 
-	/// <summary>
-	/// Gets the size x.
-	/// </summary>
-	/// <returns>The size x.</returns>
-	public int Size_x{
-		get{
-			return size_x;
-		}
-		set{
-			size_x = value;
-		}
-	}
+    /// <summary>
+    /// Gets the size x.
+    /// </summary>
+    /// <returns>The size x.</returns>
+    public int Size_x
+    {
+        get
+        {
+            return size_x;
+        }
+        set
+        {
+            size_x = value;
+        }
+    }
 
-	// z number of grids in vertical
-	private int size_z;
+    // z number of grids in vertical
+    private int size_z;
 
-	/// <summary>
-	/// Gets the size z.
-	/// </summary>
-	/// <returns>The size z.</returns>
-	public int Size_z{
-		get{
-			return size_z;
-		}
-		set{
-			size_z = value;
-		}
-	}
+    /// <summary>
+    /// Gets the size z.
+    /// </summary>
+    /// <returns>The size z.</returns>
+    public int Size_z
+    {
+        get
+        {
+            return size_z;
+        }
+        set
+        {
+            size_z = value;
+        }
+    }
 
-	// grid map contains the index of the zone
-	private int[][] grid;
+    // grid map contains the index of the zone
+    private int[][] grid;
 
-	/// <summary>
-	/// Gets or sets the grid.
-	/// </summary>
-	/// <value>The grid.</value>
-	public int[][] Grid{
-		get{
-			return grid;
-		}
-		set{
-			grid = value;
-		}
-	}
+    /// <summary>
+    /// Gets or sets the grid.
+    /// </summary>
+    /// <value>The grid.</value>
+    public int[][] Grid
+    {
+        get
+        {
+            return grid;
+        }
+        set
+        {
+            grid = value;
+        }
+    }
 
-	// grid map of the walkable path
-	private bool[][] path;
+    // grid map of the walkable path
+    private bool[][] path;
 
-	/// <summary>
-	/// Gets or sets the path.
-	/// </summary>
-	/// <value>The path.</value>
-	public bool [][] Path {
-		get {
-			return path;
-		}
-		set {
-			path = value;
-		}
-	}
+    /// <summary>
+    /// Gets or sets the path.
+    /// </summary>
+    /// <value>The path.</value>
+    public bool[][] Path
+    {
+        get
+        {
+            return path;
+        }
+        set
+        {
+            path = value;
+        }
+    }
 
-	// Store human references here for easy access
-	private Dictionary<int, GameObject> humans = new Dictionary<int, GameObject>();
-	/// <summary>
-	/// Gets or sets the humans.
-	/// </summary>
-	/// <value>The humans.</value>
-	public Dictionary<int, GameObject> Humans {
-		get {
-			return humans;
-		}
-		set {
-			humans = value;
-		}
-	}
+    // Store human references here for easy access
+    private Dictionary<int, GameObject> humans = new Dictionary<int, GameObject>();
+    /// <summary>
+    /// Gets or sets the humans.
+    /// </summary>
+    /// <value>The humans.</value>
+    public Dictionary<int, GameObject> Humans
+    {
+        get
+        {
+            return humans;
+        }
+        set
+        {
+            humans = value;
+        }
+    }
 
     private void Start()
     {
         InvokeRepeating("GetCityUpdate", 60.0f, 60.0f);
     }
 
-    private void GetInitialCityData()
-    {
-
-    }
-
     private void GetCityUpdate()
     {
+        int lastPartialUpdate = lastUpdate ?? -1;
 
+        PartialSimulationUpdateRequest partialUpdate = new PartialSimulationUpdateRequest
+        {
+            RequestType = "update",
+            FullUpdate = false,
+            LastUpdate = lastPartialUpdate
+        };
+
+        partialCityState = NetworkConnectionHandler.WriteForServer(JsonUtility.ToJson(partialUpdate));
+        updateTheCity = true;
     }
 
     private void GetDatabaseResource()
@@ -242,70 +272,83 @@ public class CityDataManager : MonoBehaviour {
     /// <summary>
     // Awake this instance.
     /// </summary>
-    void Awake () {
+    void Awake()
+    {
 
         // TODO: Server request initial
+        SimulationUpdateRequest fullRequest = new SimulationUpdateRequest
+        {
+            RequestType = "update",
+            FullUpdate = true
+        };
+
+        initialCityState = NetworkConnectionHandler.WriteForServer(JsonUtility.ToJson(fullRequest));
 
         initiateCityData();
 
-		systemStartedTimeStamp =  System.DateTime.Now.Minute;
-		updateTheCity = false;
-		if(turnOnTestGrid){ // if turned on for test, initiate test grid
-			initiateGridForTest();
-		}
+        systemStartedTimeStamp = System.DateTime.Now.Minute;
+        updateTheCity = false;
+        if (turnOnTestGrid)
+        { // if turned on for test, initiate test grid
+            initiateGridForTest();
+        }
         // else {  // else general grid
-		//	initiateGrid ();
-		//}
-    
+        //	initiateGrid ();
+        //}
+
         Debug.Log(cityData.GridLength);
-	}
+    }
 
-	/// <summary>
-	/// Update this instance.
-	/// </summary>
-	void Update(){
-		/// Will be used in the future
-		systemCurrentTimeStamp = System.DateTime.Now.Minute;
+    /// <summary>
+    /// Update this instance.
+    /// </summary>
+    void Update()
+    {
+        /// Will be used in the future
+        systemCurrentTimeStamp = System.DateTime.Now.Minute;
 
-        // TODO: request update
+        if (updateTheCity)
+        {
 
+            //Parse partialCityState string to json, set the update last update to the nethours field of partialcityupdate
+            updateTheCity = false;
+        }
 
-		if(updateTheCity){
+        // Debug.Log(systemStartedTimeStamp);
+        // Debug.Log(systemCurrentTimeStamp);
+    }
 
-			//call the updates
-
-			updateTheCity = false;
-		}
-
-		// Debug.Log(systemStartedTimeStamp);
-		// Debug.Log(systemCurrentTimeStamp);
-	}
-
-    public void initiateCityData(){
+    public void initiateCityData()
+    {
         size_z = cityData.GridLength;
         size_x = cityData.GridWidth;
 
         grid = new int[size_x][];
 
         // initalize 2d array
-        for (int x = 0; x < grid.Length; x++) {
-            grid [x] = new int[size_z]; 
+        for (int x = 0; x < grid.Length; x++)
+        {
+            grid[x] = new int[size_z];
         }
 
         // iterates grid and assign the zone
-        for(int x = 0; x < size_x; x++){
-            for (int z = size_z -1; z >= 0; z--) {
+        for (int x = 0; x < size_x; x++)
+        {
+            for (int z = size_z - 1; z >= 0; z--)
+            {
                 grid[x][z] = -1;
             }
         }
 
         // assign road to grid        
-        foreach(Point point in cityData.NewRoads){
+        foreach (Point point in cityData.NewRoads)
+        {
             grid[point.x][point.z] = 0;
         }
 
         // assign building to grid
-        foreach(NewBuilding building in cityData.NewBuildings){
+        foreach (NewBuilding building in cityData.NewBuildings)
+        {
 
             // Debug.Log(building);
             // Debug.Log((string)building.Type + " "
@@ -316,117 +359,129 @@ public class CityDataManager : MonoBehaviour {
             //         + building.IsTall);
 
             int type = -1;
-            switch(building.Type[0]){
+            switch (building.Type[0])
+            {
                 case 'H':
                     type = 1;
                     // Debug.Log("Res assigned: " + building.id + " " + building.Point.x + ", " + building.Point.z);
-                break;
+                    break;
 
                 case 'C':
                     type = 2;
                     // Debug.Log("Comm assigned: " + building.id + " " + building.Point.x + ", " + building.Point.z);
-                break;
+                    break;
 
                 case 'I':
                     type = 3;
                     // Debug.Log("Indst assigned: " + building.id + " " + building.Point.x + ", " + building.Point.z);
-                break;
+                    break;
 
                 default:
                     type = -1;
-                break;
+                    break;
             }
 
-            if(building.Point.x < size_x || building.Point.z < size_z){
+            if (building.Point.x < size_x || building.Point.z < size_z)
+            {
                 grid[building.Point.x][building.Point.z] = type;
-            } else {
+            }
+            else
+            {
                 Debug.Log("CityDataManager: building.Point is out of bound!!");
             }
         }
     }
 
-	/// <summary>
-	/// Initials the grid.
-	/// </summary>
-	public void initiateGrid() {
-		grid = new int[size_x][];
+    /// <summary>
+    /// Initials the grid.
+    /// </summary>
+    public void initiateGrid()
+    {
+        grid = new int[size_x][];
 
-		// initalize 2d array
-		for (int x = 0; x < grid.Length; x++) {
-			grid [x] = new int[size_z]; 
-		}
+        // initalize 2d array
+        for (int x = 0; x < grid.Length; x++)
+        {
+            grid[x] = new int[size_z];
+        }
 
-		// iterates grid and assign the zone
-		for(int x = 0; x < size_x; x++){
-			for (int z = size_z -1; z >= 0; z--) {
-				grid [x][z] = UnityEngine.Random.Range (0, 4);
+        // iterates grid and assign the zone
+        for (int x = 0; x < size_x; x++)
+        {
+            for (int z = size_z - 1; z >= 0; z--)
+            {
+                grid[x][z] = UnityEngine.Random.Range(0, 4);
 
-				if(grid[x][z] == 2)
-					grid[x][z] = 0;
-			}
-		}
-	}
-
-
-	/// <summary>
-	/// Initiate grid for test
-	/// </summary>
-	public void initiateGridForTest(){		
-
-		// initializing and assigning arrays
-		grid = new[] {
-			new int [] { 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 3, 3, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 3, 3, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 3, 3, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 3, 3, 3, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 3, 3, 3, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 3, 3, 3, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 1, 1},
-			new int [] { 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1}
-		};
-
-		// set the size to fit into array of
-		size_x = grid.GetLength(0);
-		size_z = grid[0].GetLength(0);
-	}
-
-	/// <summary>
-	/// Gets the index of X.
-	/// </summary>
-	/// <returns>The index of X.</returns>
-	/// <param name="x">The x coordinate.</param>
-	/// <param name="z">The z coordinate.</param>
-	public int getIndexOfXZ(int x, int z){
-
-		if (x > size_x && z > size_z) {
-			return -1;
-		}
-
-		return grid[x][z];
-	}
+                if (grid[x][z] == 2)
+                    grid[x][z] = 0;
+            }
+        }
+    }
 
 
-	/// <summary>
-	/// Turns on updateTheCity to update city 
-	/// </summary>
-	public void noticeUpdate(){
-		updateTheCity = true;
-	}
+    /// <summary>
+    /// Initiate grid for test
+    /// </summary>
+    public void initiateGridForTest()
+    {
+
+        // initializing and assigning arrays
+        grid = new[] {
+            new int [] { 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 3, 3, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 3, 3, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 3, 3, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 3, 3, 3, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 3, 3, 3, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 3, 3, 3, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 3, 3, 3, 3, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 1, 1},
+            new int [] { 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 3, 3, 3, 1, 1}
+        };
+
+        // set the size to fit into array of
+        size_x = grid.GetLength(0);
+        size_z = grid[0].GetLength(0);
+    }
+
+    /// <summary>
+    /// Gets the index of X.
+    /// </summary>
+    /// <returns>The index of X.</returns>
+    /// <param name="x">The x coordinate.</param>
+    /// <param name="z">The z coordinate.</param>
+    public int getIndexOfXZ(int x, int z)
+    {
+
+        if (x > size_x && z > size_z)
+        {
+            return -1;
+        }
+
+        return grid[x][z];
+    }
+
+
+    /// <summary>
+    /// Turns on updateTheCity to update city 
+    /// </summary>
+    public void noticeUpdate()
+    {
+        updateTheCity = true;
+    }
 }
