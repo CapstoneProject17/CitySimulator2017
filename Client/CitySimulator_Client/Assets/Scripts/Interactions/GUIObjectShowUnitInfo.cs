30ef0e1b5bdf7135c9deebf2beca90d55907372c
Benjamin <haomingkai@gmail.com>
2017-11-28 16:54:27 -0800

    Updated ShowUnitInfo for GUID v5

--- Client/CitySimulator_Client/Assets/Scripts/Interactions/GUIObjectShowUnitInfo.cs
+++ Client/CitySimulator_Client/Assets/Scripts/Interactions/GUIObjectShowUnitInfo.cs
@@ -23,6 +23,11 @@ public class GUIObjectShowUnitInfo : Interaction
     //public TextMesh info2; // TODO: something needs to be added
     //public Sprite ProfilePic;    // the profile picture of units
 
+    // JSON dummy String for testing
+    private static string jsonString = "{\"GridLength\":99,\"GridWidth\":58,\"netHours\":0,\"NewRoads\":[{\"x\":27,\"z\":49},{\"x\":27,\"z\":56},{\"x\":28,\"z\":49},{\"x\":28,\"z\":56},{\"x\":29,\"z\":49},{\"x\":29,\"z\":56},{\"x\":30,\"z\":49},{\"x\":30,\"z\":56},{\"x\":27,\"z\":50},{\"x\":30,\"z\":50},{\"x\":27,\"z\":51},{\"x\":30,\"z\":51},{\"x\":27,\"z\":52},{\"x\":30,\"z\":52},{\"x\":27,\"z\":53},{\"x\":30,\"z\":53},{\"x\":27,\"z\":54},{\"x\":30,\"z\":54},{\"x\":27,\"z\":55},{\"x\":30,\"z\":55},{\"x\":24,\"z\":56},{\"x\":24,\"z\":63},{\"x\":25,\"z\":56},{\"x\":25,\"z\":63},{\"x\":26,\"z\":56},{\"x\":26,\"z\":63},{\"x\":27,\"z\":63},{\"x\":24,\"z\":57},{\"x\":27,\"z\":57},{\"x\":24,\"z\":58},{\"x\":27,\"z\":58},{\"x\":24,\"z\":59},{\"x\":27,\"z\":59},{\"x\":24,\"z\":60},{\"x\":27,\"z\":60},{\"x\":24,\"z\":61},{\"x\":27,\"z\":61},{\"x\":24,\"z\":62},{\"x\":27,\"z\":62},{\"x\":21,\"z\":63},{\"x\":21,\"z\":70},{\"x\":22,\"z\":63},{\"x\":22,\"z\":70},{\"x\":23,\"z\":63},{\"x\":23,\"z\":70},{\"x\":24,\"z\":70},{\"x\":21,\"z\":64},{\"x\":24,\"z\":64},{\"x\":21,\"z\":65},{\"x\":24,\"z\":65},{\"x\":21,\"z\":66},{\"x\":24,\"z\":66},{\"x\":21,\"z\":67},{\"x\":24,\"z\":67},{\"x\":21,\"z\":68},{\"x\":24,\"z\":68},{\"x\":21,\"z\":69},{\"x\":24,\"z\":69}],\"NewBuildings\":[{\"id\":\"24132329-e85a-4072-b9c8-1dab463b8443\",\"Name\":\"Pacocha Inc\",\"Point\":{\"x\":28,\"z\":54},\"Type\":\"I\",\"Rating\":0,\"IsTall\":true},{\"id\":\"0a6a8518-fc33-4d7d-bf88-ef7464f72d5e\",\"Name\":\"Hilll, Kohler and Effertz\",\"Point\":{\"x\":25,\"z\":59},\"Type\":\"C\",\"Rating\":0,\"IsTall\":true},{\"id\":\"43018e9e-b03b-45d1-b214-ae7a623d5a8a\",\"Name\":\"Residence\",\"Point\":{\"x\":22,\"z\":69},\"Type\":\"H\",\"Rating\":0,\"IsTall\":true}]}";
+    // deserialize json to object
+    private CityData cityData = JsonUtility.FromJson<CityData>(jsonString);
+
     /// <summary>
     /// override Select() method
     /// </summary>
@@ -40,8 +45,19 @@ public class GUIObjectShowUnitInfo : Interaction
         //GUID.text = guid;
         //info1.text = "test2";
         //info2.text = "test3";
-        Guid g = Guid.NewGuid();
-        GUID.text = g.ToString();
+        //Guid g = Guid.NewGuid();
+        //GUID.text = g.ToString();
+
+        // TODO: interact with database
+        foreach (NewBuilding building in cityData.NewBuildings)
+        {
+            GUID.text = building.id + "\n"
+                + building.Name + "\n"
+                + building.Point.ToString() + "\n"
+                + building.Type + "\n"
+                + building.Rating.ToString() + "\n"
+                + building.IsTall.ToString();
+        }
     }
 
     /// <summary>
