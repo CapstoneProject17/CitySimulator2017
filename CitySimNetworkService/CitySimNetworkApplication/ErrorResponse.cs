e7043c0b1e7ee39ccc42d4d58f6119d9fa8cadc2
Kevin Mitchell <you@example.com>
2017-11-28 16:37:19 -0800

    further comments

--- CitySimNetworkService/CitySimNetworkApplication/ErrorResponse.cs
+++ CitySimNetworkService/CitySimNetworkApplication/ErrorResponse.cs
@@ -7,14 +7,21 @@ using System.Threading.Tasks;
 namespace CitySimNetworkingApplication
 {
 	/// <summary>
-	/// Holds the error that will be sent to client upon invalid request
+	/// Holds the error that will be sent to client upon invalid requests
 	/// </summary>
 	/// <author>
 	/// Kevin
 	/// </author>
 	public class ErrorResponse
 	{
+		/// <summary>
+		/// Http response type: error/data/etc
+		/// </summary>
 		public string type = "error";
+
+		/// <summary>
+		/// Message that the Http Error Response will contain
+		/// </summary>
 		public string Message { get; set; }
 	}
 }
